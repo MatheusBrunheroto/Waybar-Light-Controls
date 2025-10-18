ba3c43ad12110b9cf67f4583f50ae0b710e9d8c7
Matheus <matheus.brunheroto@gmail.com>
2025-10-17 22:24:34 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,12 +31,14 @@ if [ -d "$HYPR_DIR/$TARGET" ]; then
 	echo -e "\e[33m[!] WARNING: $HYPR_DIR/$TARGET already exists. Removing for reinstallation... \e[0m"
         rm -rf "$HYPR_DIR/$TARGET"
 fi
+sleep 0.5
 if [ -d "$WAYBAR_DIR/$TARGET" ]; then
         echo -e "\e[33m[!] WARNING: $WAYBAR_DIR/$TARGET already exists. Removing for reinstallation... \e[0m"
         rm -rf "$WAYBAR_DIR/$TARGET"
 fi
 
-
+sleep 1
+echo -e " "
 
 create_directory() {
 
@@ -50,6 +52,7 @@ create_directory() {
     else
         echo "[+] Creating $TARGET at $BASE_DIR"
     fi
+    sleep 0.5
 
     # Create directories at BASE_DIR
     mkdir -p "$DIR"
@@ -60,11 +63,22 @@ create_directory() {
         exit 1
     fi    
 
-
+    sleep 1
+    echo -e " "
 }
 create_directory "$HYPR_DIR"
 create_directory "$WAYBAR_DIR"
 
 cp -r "$REPO_DIR/scripts/hypr/"* "$HYPR_DIR/$TARGET"
-cp -r "$REPO_DIR/scripts/waybar/"* "$WAYBAR_DIR/$TARGET"
+echo -e "\e[32m[✓] Files moved to $HYPR_DIR/$TARGET successfully!\e[0m"
+sleep 1
 
+cp -r "$REPO_DIR/scripts/waybar/"* "$WAYBAR_DIR/$TARGET"
+echo -e "\e[32m[✓] Files moved to $WAYBAR_DIR/$TARGET successfully!\e[0m"
+sleep 1.5
+
+echo -e "\n\e[32m[✓] Installation complete!\e[0m"
+echo "Next steps:"
+echo "  1. Configure your Waybar to use the scripts."
+echo "  2. Restart Waybar or your session to apply changes."
+echo "Full guide available at: https://github.com/MatheusBrunheroto/Waybar-Light-Controls"
